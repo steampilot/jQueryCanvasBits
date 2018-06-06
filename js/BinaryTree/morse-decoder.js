b62f72d14e2588c220988281b1622db819dc6179
Steampilot <info@steampilot.ch>
2018-06-06 11:13:50 +0200

    WIP MorseCode Tree

--- js/BinaryTree/morse-decoder.js
+++ js/BinaryTree/morse-decoder.js
@@ -1,72 +1,125 @@
 /**
  * Created by steam on 06.06.2018.
  */
-var tree;
+var morseTree;
 
 function setup() {
-    tree = new Tree();
-    var alphabet = {
-        A:'.-',
-        B:'-...',
-        C:'-.-.',
-        D:'-.-.',
-        E:'.',
-        F:'..-.',
-        G:'--.',
-        H:'...',
-        I:'.---',
-        J:'-.-',
-        K:'-.-',
-        L:'.-..',
-        M:'--',
-        N:'-.',
-        O:'---',
-        P:'.--.',
-        Q:'--.-',
-        R:'.-.',
-        S:'...',
-        T:'-',
-        U:'..-',
-        V:'...-',
-        W:'.--',
-        X:'-..-',
-        Y:'-.--',
-        Z:'--..',
-        1:'.----',
-        2:'..---',
-        3:'...--',
-        4:'....-',
-        5:'.....',
-        6:'-....',
-        7:'--...',
-        8:'---..',
-        9:'----.',
-        0:'-----',
-    };
-    tree.addChar('A', '.-');
-    tree.addChar('B', '-...');
-    tree.addChar('C', '-.-.');
-    console.log(tree);
-    tree.traverse();
+    morseTree = new MorseTree();
+    var Alphabet = new Array();
+    Alphabet['A'] = '.-|';
+    Alphabet['B'] = '-...|';
+    Alphabet['C'] = '-.-.|';
+    Alphabet['D'] = '-..|';
+    Alphabet['E'] = '.|';
+    Alphabet['F'] = '..-.|';
+    Alphabet['G'] = '--.|';
+    Alphabet['H'] = '....|';
+    Alphabet['I'] = '..|';
+    Alphabet['J'] = '-.-';
+    Alphabet['K'] = '-.-|';
+    Alphabet['L'] = '.-..|';
+    Alphabet['M'] = '--|';
+    Alphabet['N'] = '-.|';
+    Alphabet['O'] = '---|';
+    Alphabet['P'] = '.--.';
+    Alphabet['Q'] = '--.-|';
+    Alphabet['R'] = '.-.|';
+    Alphabet['S'] = '...|';
+    Alphabet['T'] = '-|';
+    Alphabet['U'] = '..-|';
+    Alphabet['V'] = '...-|';
+    Alphabet['W'] = '.--|';
+    Alphabet['X'] = '-..-|';
+    Alphabet['Y'] = '-.--|';
+    Alphabet['Z'] = '--..|';
+    Alphabet['1'] = '.----|';
+    Alphabet['2'] = '..---|';
+    Alphabet['3'] = '...--|';
+    Alphabet['4'] = '....-|';
+    Alphabet['5'] = '.....|';
+    Alphabet['6'] = '-....|';
+    Alphabet['7'] = '--...|';
+    Alphabet['8'] = '---..|';
+    Alphabet['9'] = '----.|';
+    Alphabet['0'] = '-----|';
+    Alphabet['?'] = '..--..|';
+    Alphabet['.'] = '.-.-.-|';
+    Alphabet[','] = '--..--|';
+    Alphabet['='] = '-...-|';
+    Alphabet['+'] = '.-.-.|';
+    let index;
+    for (index in Alphabet) {
+        let char = new Char(index, Alphabet[index]);
+        morseTree.addChar(char)
+    }
+    console.log(morseTree);
+
 
 }
+
 setup();
 
-function Dot() {
-    this.sound = null;
-    this.symbol = null;
-}
-function Dash() {
-    this.sound = null;
-    this.symbol = null;
+function Char(char, morse) {
+    this.char = char;
+    this.morse = morse;
 }
 
-Tree.prototype.addChar = function (value) {
-    var node = new Node(value);
+function MorseTree() {
+    this.root = null;
+}
+
+MorseTree.prototype.addChar = function (char) {
+    var node = new MorseNode(char);
     if (this.root === null) {
         this.root = node;
     } else {
         this.root.addNode(node)
     }
 
+};
+
+
+function MorseNode(char) {
+    this.char = char;
+    this.chain = char.morse;
+    this.dot = null;
+    this.dash = null;
+}
+
+MorseNode.prototype.addNode = function (node) {
+    let symbol = this.chain.charAt(0);
+    if (symbol === '|') {
+        this.char = node.char;
+        this.chain = null;
+    } else {
+        this.chain = this.chain.substr(1);
+        if (symbol === '.') {
+            if (this.dot === null) {
+                this.dot.addNode(node);
+            } else {
+                this.dot.visit(node);
+            }
+        }
+        if (symbol === '-'){
+            if (this.dash === null) {
+                this.dash.addNode(node);
+            } else {
+                this.dash.visit(node);
+            }
+        }
+    }
+};
+MorseNode.prototype.visit = function (node) {
+    let symbol = this.chain.charAt(0);
+    if (symbol === '|') {
+        this.char = node.char;
+        this.chain = null;
+    }
+    if (this.dot !== null) {
+        this.dot.visit();
+    }
+    console.log(this.morse + ' = ' + this.char);
+    if (this.dash !== null) {
+        this.dash.visit();
+    }
 };
